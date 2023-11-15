6fb7a33bcf7c5efd73859fb80b2fa7945c44af69
kisyular <rellika@kisyula.com>
2023-11-15 23:04:49 +0300

    ADDED PART OF THE AUTHENTICATION TO THE APP

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const ClerkLayout = ({ children }: { children: React.ReactNode }) => {
+	return (
+		<div className='h-full flex items-center justify-center bg-black'>
+			{children}
+		</div>
+	)
+}
+
+export default ClerkLayout
