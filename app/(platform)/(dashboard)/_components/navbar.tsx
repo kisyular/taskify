bb9cc4b18df1884c7bd8f7c96879083e6154bd06
kisyular <rellika@kisyula.com>
2023-11-22 14:20:56 +0300

    MINOR CHANGES TO THE NAVBAR TO ENABLE TO ADD BOARDS FROM NAVBAR

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -7,6 +7,7 @@ import { Plus } from 'lucide-react'
 import { useOrganization } from '@clerk/nextjs'
 import { Skeleton } from '@/components/ui/skeleton'
 import { MobileSidebar } from './mobile-sidebar'
+import { FormPopover } from '@/components/forms/form-popover'
 
 const Navbar = () => {
 	const { isLoaded: isLoadedOrg } = useOrganization()
@@ -18,20 +19,24 @@ const Navbar = () => {
 				<div className='hidden md:flex'>
 					<Logo />
 				</div>
-				<Button
-					variant='default'
-					size='sm'
-					className='rounded-sm hidden md:block h-auto  py-1.5 px-2'
-				>
-					Create
-				</Button>
-				<Button
-					variant='default'
-					size='sm'
-					className='rounded-sm block md:hidden'
-				>
-					<Plus className='h-4 w-4' />
-				</Button>
+				<FormPopover align='start' side='bottom' sideOffset={18}>
+					<Button
+						variant='default'
+						size='sm'
+						className='rounded-sm hidden md:block h-auto  py-1.5 px-2'
+					>
+						Create
+					</Button>
+				</FormPopover>
+				<FormPopover>
+					<Button
+						variant='default'
+						size='sm'
+						className='rounded-sm block md:hidden'
+					>
+						<Plus className='h-4 w-4' />
+					</Button>
+				</FormPopover>
 			</div>
 			<div className='ml-auto flex items-center gap-x-2 '>
 				{!isLoadedOrg && (
